042678aebccbbb90a841687c7052cea75477ed92
mohan <mohannani2020@gmail.com>
2025-07-16 14:39:24 +0530

    initial commit

---
+++ src/main/java/org/techm/samples/Product_catalog.java
@@ -0,0 +1,12 @@
+package org.techm.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Product_catalog {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(Product_catalog.class, args);
+	}
+}
---
+++ src/main/java/org/techm/samples/entity/Role.java
@@ -0,0 +1,7 @@
+package org.techm.samples.entity;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
+
