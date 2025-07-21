91fe72fa894470a31a8f2ad3e3eaaac2563daf63
manas <manas.penti@gmail.com>
2025-07-21 11:09:18 +0530

    Updated Users entity and created categories entity

---
+++ src/main/java/org/techm/samples/entity/Categories.java
@@ -0,0 +1,65 @@
+package org.techm.samples.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categories {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id;
+	private String name;
+	private String description;
+	private LocalDate created_at;
+	private LocalDate updated_at;
+	
+	public long getId() {
+		return Id;
+	}
+	public void setId(long id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(LocalDate created_at) {
+		this.created_at = created_at;
+	}
+	public LocalDate getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(LocalDate updated_at) {
+		this.updated_at = updated_at;
+	}
+	public Categories(String name, String description, LocalDate created_at, LocalDate updated_at) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+	public Categories() {
+		super();
+		
+	}
+	
+	
+		
+
+}
--- src/main/java/org/techm/samples/entity/User.java
+++ src/main/java/org/techm/samples/entity/User.java
@@ -1,5 +1,7 @@
 package org.techm.samples.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -15,7 +17,47 @@ public class User {
 	
 	private String username;
 	private String password;
+	private String email;
+	private LocalDate created_at;
+	private LocalDate updated_at;
 	
+	public User() {
+		super();
+	}
+
+	public User(String username, String email, LocalDate created_at, LocalDate updated_at, Role role) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDate created_at) {
+		this.created_at = created_at;
+	}
+
+	public LocalDate getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(LocalDate updated_at) {
+		this.updated_at = updated_at;
+	}
+
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
